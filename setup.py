f0880d86b5ac3ecd3b5b35d5708d4b8a75996f44
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2018-11-03 15:05:04 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Setup script for netflows
+
+you can install netflows with
+
+python setup.py install
+"""
+
+from setuptools import setup
+import os
+import sys
+
+if sys.argv[-1] == 'setup.py':
+    print("To install, run 'python setup.py install'")
+    print()	
