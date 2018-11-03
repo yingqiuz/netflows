a2583035d263c6aa69467881d377e4e1f7d666e5
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2018-11-03 15:41:20 -0400

    add __init__.py

---
+++ netflows/__init__.py
@@ -0,0 +1,36 @@
+"""
+netflows
+========
+
+netflows is a Python package for optimal traffic assignment / Wardrop Equilibrium on brain or transportation networks.
+
+Source::
+
+    https://github.com/yingqiuz/netflows
+    
+Simple example
+---------------------
+YBC
+
+License
+---------------------
+TBC
+"""
+
+from __future__ import absolute_import
+
+import sys
+if sys.version_info[:2] < (2, 7):
+    s = "Python 2.7 or later is required for netflows (%d.%d detected). "
+    raise ImportError(m % sys.version_info)
+del sys
+
+# release data
+# TBC
+
+from Graph import Graph
+from costfuncs import *
+from solvefuncs import *
+
+
+
