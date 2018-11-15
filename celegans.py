1d9c3d6dd0cf5b8b66816009a8012d007c7c2ca1
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2018-11-14 21:45:59 -0500

    update celegan script

--- celegans.py
+++ celegans.py
@@ -10,20 +10,20 @@ import pickle
 with open('celegans_conn.pickle', 'rb') as f:
     celegans = pickle.load(f)
 
-celegans_adj1 = celegans['celegans_adj_1']
+#celegans_adj1 = celegans['celegans_adj_1']
 celegans_adj2 = celegans['celegans_adj_2']
 celegans_dist = celegans['celegans_dist']
 
-G_celegans1 = Graph(adj=celegans_adj1, dist=celegans_dist, weights=celegans_adj1)
-
-for row in range(G_celegans1.adj.shape[0]):
-    for col in range(G_celegans1.adj.shape[1]):
-        if row == col:
-            continue
-        else:
-            print('now computing the WE flow of node pair (%d, %d)' % (row, col))
-            WElinearsolve(G_celegans1, row, col, tol=1e-7, maximum_iter=100000, cutoff=None)
-            #WEaffinesolve(G_celegans1, row, col, tol=1e-7, maximum_iter=100000, cutoff=None)
+# G_celegans1 = Graph(adj=celegans_adj1, dist=celegans_dist, weights=celegans_adj1)
+#
+# for row in range(G_celegans1.adj.shape[0]):
+#     for col in range(G_celegans1.adj.shape[1]):
+#         if row == col:
+#             continue
+#         else:
+#             print('now computing the WE flow of node pair (%d, %d)' % (row, col))
+#             WElinearsolve(G_celegans1, row, col, tol=1e-7, maximum_iter=100000, cutoff=None)
+#             #WEaffinesolve(G_celegans1, row, col, tol=1e-7, maximum_iter=100000, cutoff=None)
 
 G_celegans2 = Graph(adj=celegans_adj2, dist=celegans_dist, weights=celegans_adj2)
 
@@ -37,6 +37,6 @@ for row in range(G_celegans2.adj.shape[0]):
             WEaffinesolve(G_celegans2, row, col, tol=1e-7, maximum_iter=100000, cutoff=None)
 
 with open('G_celegans.pickle', 'wb') as f:
-    pickle.dump({'G_celegans1': G_celegans1, 'G_celegans2':G_celegans2}, f)
+    pickle.dump({'G_celegans2':G_celegans2}, f)
 
 
