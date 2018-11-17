01841dace8c3c8f4f111ec4a326b2b6771a28e83
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2018-11-17 13:56:08 -0500

    update main script

--- single_pair_WEflow.py
+++ single_pair_WEflow.py
@@ -5,38 +5,38 @@ from netflows import Graph
 from netflows.funcs import WElinearsolve, WEaffinesolve, WEbprsolve
 
 
-
 def construct_data():
-    parameter_dict = {}
+    parameters = {}
     # python single_pair_flow.py G=filename source=s target=t
     for user_input in sys.argv[1:]:
         varname = user_input.split("=")[0]
         varvalue = user_input.split("=")[1]
-        parameter_dict[varname] = varvalue
+        parameters[varname] = varvalue
     # load adj data
-    with open('data/' + parameter_dict['G']+'_adj.pickle', 'rb') as f:
+    with open('data/' + parameters['G'] + '_adj.pickle', 'rb') as f:
         adj = pickle.load(f)
     # load dist data
     try:
-        with open('data/' + parameter_dict['G']+'_dist.pickle', 'rb') as f:
+        with open('data/' + parameters['G'] + '_dist.pickle', 'rb') as f:
             dist = pickle.load(f)
     except:
         dist = np.zeros(adj.shape)
 
-    return adj, dist, parameter_dict
+    return adj, dist, parameters
+
 
 if __name__ == '__main__':
+    adj, dist, parameters = construct_data()
+    G = Graph(adj=adj, dist=dist, weights=adj)
 
-    adj, dist, parameter_dict = construct_data()
-    G = Graph(adj = adj, dist = dist, weights = adj)
+    s = parameters['source']
+    t = parameters['target']
 
-    s = parameter_dict['source']
-    t = parameter_dict['target']
+    x, allflows, total_cost_sum, total_cost = WElinearsolve(G, int(s), int(t), cutoff=None, maximum_iter=100000,
+                                                            tol=1e-8)
 
-    x, allflows, total_cost_sum, total_cost = WElinearsolve(G, int(s), int(t), cutoff=None, maximum_iter=100000, tol=1e-8)
-
-    filename = 'results/'+ parameter_dict['G']+ '_WE_' + s + '_' + t + '.pickle'
+    filename = 'results/' + parameters['G'] + '_WE_' + s + '_' + t + '.pickle'
 
     with open(filename, 'wb') as f:
-        pickle.dump({'allflows':allflows, 'total_cost_sum':total_cost_sum, 'total_cost':total_cost},
+        pickle.dump({'allflows': allflows, 'total_cost_sum': total_cost_sum, 'total_cost': total_cost},
                     f, protocol=pickle.HIGHEST_PROTOCOL)
