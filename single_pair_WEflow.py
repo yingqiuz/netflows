ae9ab732566a90ed7cef96a6de6b1dbbc84bc7c9
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2018-11-17 14:24:25 -0500

    add new conn files and update main script

--- single_pair_WEflow.py
+++ single_pair_WEflow.py
@@ -7,17 +7,17 @@ from netflows.funcs import WElinearsolve, WEaffinesolve, WEbprsolve
 
 def construct_data():
     parameters = {}
-    # python single_pair_flow.py G=filename source=s target=t
+    # python single_pair_flow.py adj=filename dist=filename source=s target=t
     for user_input in sys.argv[1:]:
         varname = user_input.split("=")[0]
         varvalue = user_input.split("=")[1]
         parameters[varname] = varvalue
     # load adj data
-    with open('data/' + parameters['G'] + '_adj.pickle', 'rb') as f:
+    with open('data/' + parameters['adj'] + '_adj.pickle', 'rb') as f:
         adj = pickle.load(f)
     # load dist data
     try:
-        with open('data/' + parameters['G'] + '_dist.pickle', 'rb') as f:
+        with open('data/' + parameters['dist'] + '_dist.pickle', 'rb') as f:
             dist = pickle.load(f)
     except:
         dist = np.zeros(adj.shape)
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     x, allflows, total_cost_sum, total_cost = WElinearsolve(G, int(s), int(t), cutoff=None, maximum_iter=100000,
                                                             tol=1e-8)
 
-    filename = 'results/' + parameters['G'] + '_WE_' + s + '_' + t + '.pickle'
+    filename = 'results/' + parameters['adj'] + '_WE_' + s + '_' + t + '.pickle'
 
     with open(filename, 'wb') as f:
         pickle.dump({'allflows': allflows, 'total_cost_sum': total_cost_sum, 'total_cost': total_cost},
