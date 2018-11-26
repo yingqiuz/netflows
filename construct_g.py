eb1cc42c762be7e75330e70b64aaba7ae9102fcf
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2018-11-25 20:08:04 -0500

    integrate object G

--- construct_g.py
+++ construct_g.py
@@ -4,6 +4,7 @@
 import sys,os
 import numpy as np
 import pickle
+from netflows import Graph
 
 
 def read_data(model):
@@ -17,20 +18,30 @@ def read_data(model):
     with open('data/' + model + '_adj.pickle', 'rb') as f:
         adj = pickle.load(f)
 
-    return filelist, adj
+    try :
+        with open('data/' + model + '_dist.pickle', 'rb') as f:
+            dist = pickle.load(f)
+    except:
+        dist = np.ones(adj.shape)
+
+    return filelist, adj, dist
 
 if __name__ == '__main__':
 
     # read input
     model = sys.argv[1]
-    filelist, adj = read_data(model)
+    filelist, adj, dist = read_data(model)
+    G = Graph(adj=adj, dist=dist, weights=adj)
+
+    G.WEflowsLinear, G.WEflowsLinear_edge, G.WEtimeLinear, G.WEtimeLinear_edge, G.WEtimeLinear_ratio \
+        = np.zeros((5, adj.shape[0], adj.shape[1]))
 
     # initialize total flow and cost
-    total_flow = 0
-    total_flow_edge = np.zeros(adj.shape)
-    total_time = np.zeros(adj.shape)
-    total_time_edge = np.zeros(adj.shape)
-    total_time_ratio = np.zeros(adj.shape)
+    #total_flow = 0
+    #total_flow_edge = np.zeros(adj.shape)
+    #total_time = np.zeros(adj.shape)
+    #total_time_edge = np.zeros(adj.shape)
+    #total_time_ratio = np.zeros(adj.shape)
 
     for fname in filelist:
         print(fname)
@@ -49,17 +60,24 @@ if __name__ == '__main__':
         total_cost = data['total_cost']
 
         if total_cost_sum > 0:
-            total_flow += 1
-            total_flow_edge += allflows
-            total_time[int(s), int(t)] = total_cost_sum
-            total_time_edge += total_cost
-            total_time_ratio += total_cost / total_cost_sum
+            #total_flow += 1
+            G.WEflowsLinear[int(s), int(t)] = 1
+            #total_flow_edge += allflows
+            G.WEflowsLinear_edge += allflows
+            #total_time[int(s), int(t)] = total_cost_sum
+            G.WEtimeLinear[int(s), int(t)] = total_cost_sum
+            #total_time_edge += total_cost
+            G.WEtimeLinear_edge += total_cost
+            #total_time_ratio += total_cost / total_cost_sum
+            G.WEtimeLinear_ratio += total_cost / total_cost_sum
+
+    G.total_flow = G.WEflowsLinear.sum()
 
     with open('total/' + model + '_raw.pickle', 'wb') as f:
-        pickle.dump({'total_flow':total_flow, 'total_flow_edge':total_flow_edge,
-                     'total_time':total_time, 'total_time_edge':total_time_edge,
-                     'total_time_ratio':total_time_ratio},
-                    f, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(G, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
 
 
 
