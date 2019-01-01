097d7f16259a65d39a5a524b8f391d6bd5f048e9
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2019-01-01 21:14:49 +0800

    [ENH] update solvers

--- netflows/funcs/linearsolve.py
+++ netflows/funcs/linearsolve.py
@@ -1,21 +1,37 @@
-from __future__ import absolute_import
-from netflows.funcs.costfuncs import linear_cost, linear_WE_obj, linear_SO_obj
+# -*- coding: utf-8 -*-
+"""
+solve wardrop equilibrium or system optimal flow for linear cost functions k*t
+"""
 
+from netflows.utils import linear_cost, linear_so_obj
 import numpy as np
+from tqdm import tqdm
 
 
-def wardrop_equilibrium_linear_solve(G, s, t, tol=1e-12, maximum_iter=10000, cutoff=None, a=None):
+def wardrop_equilibrium_linear_solve(G, s, t, tol=1e-8, maximum_iter=10000, cutoff=None, a=None):
     """
-    single pair Wardrop Equilibrium flow
-    s: source
-    t: destination
-    tol: tolerance
-    gamma: descent speed
+    The function to solve Wardrop Equilibrium flow for a single source target pair under linear cost function setting.
+    Usage:
+
+    :param G: Graph object, storing the adjacency/weight/distance matrices
+    :param s: source node
+    :param t: target node
+    :param tol: tolerance for convergence
+    :param maximum_iter: maximum iteration times
+    :param cutoff: a scalar value that defines maximal (binary) path length, namely,
+        flows can only use paths shorter than the cutoff value
+    :param a: the parameter of linear cost function, default is distance/weights
+    :return: a tuple (x, allflows, total_cost_sum, total_cost).
+        x: the vector storing flows on each path (path formulation)
+        allflows: the matrix storing flows on each edge (edge formulation)
+        total_cost_sum: total travel time for all flows from source to target
+        total_cost: the matrix storing travel time on each edge (edge formulation)
     """
 
+    # find all possible paths from s to t that are shorter than cutoff
     if cutoff is None:
-        print("Cutoff not specified: take shortest path distance + 1 as cutoff")
-        cutoff = G._dijkstra(s, t) + 1 + 1
+        print("Cutoff is not specified: shortest path distance + 1 taken as cutoff")
+        cutoff = G.dijkstra(s, t) + 1 + 1
         if cutoff < 3:
             return
 
@@ -27,20 +43,30 @@ def wardrop_equilibrium_linear_solve(G, s, t, tol=1e-12, maximum_iter=10000, cut
     return _wardrop_equilibrium_linear_solve(G, s, t, tol, maximum_iter, allpaths, a)
 
 
-def system_optimal_linear_solve(G, s, t, tol=1e-12, maximum_iter=10000, cutoff=None, a=None):
+def system_optimal_linear_solve(G, s, t, tol=1e-8, maximum_iter=10000, cutoff=None, a=None):
     """
-    :param G:
-    :param s:
-    :param t:
-    :param tol:
-    :param maximum_iter:
-    :param cutoff:
-    :return:
+    The function to solve system optimal flow for a single source target pair under linear cost function setting.
+    Usage:
+
+    :param G: Graph object, storing the adjacency/weight/distance matrices
+    :param s: source node
+    :param t: target node
+    :param tol: tolerance for convergence
+    :param maximum_iter: maximum iteration times
+    :param cutoff: a scalar value that defines maximal (binary) path length, namely,
+        flows can only use paths shorter than the cutoff value
+    :param a: the parameter of linear cost function, default is distance/weights
+    :return: a tuple (x, allflows, total_cost_sum, total_cost).
+        x: the vector storing flows on each path (path formulation)
+        allflows: the matrix storing flows on each edge (edge formulation)
+        total_cost_sum: total travel time for all flows from source to target
+        total_cost: the matrix storing travel time on each edge (edge formulation)
     """
 
+    # find all possible paths from s to t that are shorter than cutoff
     if cutoff is None:
         print("Cutoff not specified: take shortest path distance + 1 as cutoff")
-        cutoff = G._dijkstra(s, t) + 1 + 1
+        cutoff = G.dijkstra(s, t) + 1 + 1
         if cutoff < 3:
             return
 
@@ -54,41 +80,38 @@ def system_optimal_linear_solve(G, s, t, tol=1e-12, maximum_iter=10000, cutoff=N
 
 def _wardrop_equilibrium_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
 
-    num_variables = len(allpaths) # the number of paths from s to t
-    print('there is a total of ' + str(num_variables) + ' paths')
+    num_variables = len(allpaths)  # the number of paths from s to t
+    print('A total of %d paths found from %d to %d' % (num_variables, int(s), int(t)))
 
     # x is the flow vector (path formulation)
-    x = np.ones((num_variables, )) / num_variables # initial value
+    x = np.ones((num_variables, )) / num_variables  # initial value
+
     # find equilibrium -- convex optimization
     # map paths to matrix
-    path_arrays = np.empty((0, G.adj.shape[0], G.adj.shape[1])) # list of matrix to store path flows
-
-    for path in allpaths:
-
+    path_arrays = np.empty((0, G.adj.shape[0], G.adj.shape[1]))  # list of matrix to store path flows
+    print('constructing edge formulations...')
+    for path in tqdm(allpaths, total=num_variables):
         path_array_tmp = np.zeros(G.adj.shape)
-        index_x = [path[k] for k in range(len(path)-1)] # x index of the adj matrix
-        index_y = [path[k] for k in range(1, len(path))] # y index of the adj matrix
+        index_x = [path[k] for k in range(len(path)-1)]  # x index of the adj matrix
+        index_y = [path[k] for k in range(1, len(path))]  # y index of the adj matrix
         path_array_tmp[index_x, index_y] = 1
-        path_arrays = np.append(path_arrays, path_array_tmp[np.newaxis, :], axis = 0)
+        path_arrays = np.append(path_arrays, path_array_tmp[np.newaxis, :], axis=0)
 
     # element (i, j) is the total flow on edge (i,j)
-    allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis = 0)
+    allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis=0)
     total_cost = allflows * linear_cost(allflows, a)
     total_cost_sum = total_cost.sum()
- 
-    print('The initial cost is %f, and the initial flow is ' % (total_cost_sum), x)
+    print('The initial flow is (path formulation) ', x)
+    print('The initial cost is %f' % total_cost_sum)
+    print('------------solving the Wardrop Equilibrium-------------')
 
     if num_variables < 2:
-        #G.WEflowsLinear[s, t] = 1
-        #G.WEcostsLinear[s, t] = total_cost_sum
-        #G.WEflowsLinear_edge += allflows
-        #G.WEcostsLinear_edge += total_cost
+        print('Wardrop Equilibrium flow found:', x)
+        print('The total travel time is %f' % total_cost_sum)
         return x, allflows, total_cost_sum, total_cost
 
-    print('------solve the Wardrop Equilibrium------')
-
     gradients = np.array(
-        [np.sum( linear_cost(allflows, a) * (
+        [np.sum(linear_cost(allflows, a) * (
                     path_arrays[k] * np.where(path_arrays[-1] == 0, 1, 0) - np.where(path_arrays[k] == 0, 1, 0) *
                     path_arrays[-1]))
          for k in range(num_variables - 1)]
@@ -99,7 +122,7 @@ def _wardrop_equilibrium_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
     gamma2 = np.min(np.abs((1 - x[:-1]) / gradients))
     gamma = min(gamma1, gamma2) * 2 / 3
 
-    for k in range(maximum_iter):  # maximal iteration 10000
+    for k in tqdm(range(maximum_iter)):  # maximal iteration 10000
         prev_x = np.copy(x)
         prev_gradients = np.copy(gradients)
 
@@ -107,7 +130,8 @@ def _wardrop_equilibrium_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
         x[:-1] = prev_x[:-1] - gamma * gradients
         x[-1] = 1 - np.sum(x[:-1])  # the flow in the last path
 
-        if np.sum(np.where(x < 0, 1, 0)) > 0:  # flow in at least one path is negtive
+        if np.sum(np.where(x < 0, 1, 0)) > 0:
+            # flow in at least one path is negative
             gradients[x[:-1] < 0] = prev_x[:-1][x[:-1] < 0] / gamma
             x[:-1] = prev_x[:-1] - gamma * gradients
             x[-1] = 1 - np.sum(x[:-1])  # the flow in the last path
@@ -125,47 +149,44 @@ def _wardrop_equilibrium_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
         allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis=0)
         total_cost = allflows * linear_cost(allflows, a)
         total_cost_sum = total_cost.sum()
-        
+
         # new gradients and step size
         gradients = np.array(
-            [np.sum( linear_cost(allflows, a) * (
+            [np.sum(linear_cost(allflows, a) * (
                         path_arrays[k] * np.where(path_arrays[-1] == 0, 1, 0) - np.where(path_arrays[k] == 0, 1, 0) *
                         path_arrays[-1]))
              for k in range(num_variables - 1)]
         )
 
-        if np.sum(np.where(np.abs(gradients-prev_gradients) < tol , 0, 1)) == 0:
-            print('Wardrop equilibrium found:')
-            #G.WEflowsLinear[s, t] = 1
-            #G.WEcostsLinear[s, t] = total_cost_sum
-            #G.WEflowsLinear_edge += allflows
-            #G.WEcostsLinear_edge += total_cost
-            print('Iteration %d: The total cost is %f, and the flow is ' % (k, total_cost_sum), x)
+        if np.sum(np.where(np.abs(gradients-prev_gradients) < tol, 0, 1)) == 0:
+            print('Wardrop Equilibrium flow found:', x)
+            print('Iteration %d: the total travel time is %f' % (k, total_cost_sum))
             return x, allflows, total_cost_sum, total_cost
 
-        gamma = np.inner(x[:-1] - prev_x[:-1], gradients - prev_gradients) / \
-                np.inner(gradients - prev_gradients, gradients - prev_gradients)
+        # update gamma
+        gamma = np.inner(
+            x[:-1] - prev_x[:-1], gradients - prev_gradients
+        ) / np.inner(
+            gradients - prev_gradients, gradients - prev_gradients
+        )
 
-    print('global minimum not found')
+    print('Wardrop Equilibrium flow not found')
     return
 
+
 def _system_optimal_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
-    """
-    single pair System Optimal flow
-    s: source
-    t: destination
-    tol: tolerance
-    gamma: descent speed
-    """
+
     num_variables = len(allpaths)  # the number of paths from s to t
+    print('A total of %d paths found from %d to %d' % (num_variables, int(s), int(t)))
 
     # x is the flow vector (path formulation)
     x = np.ones((num_variables,)) / num_variables  # initial value
+
     # find equilibrium -- convex optimization
     # map to matrix
     path_arrays = np.empty((0, G.adj.shape[0], G.adj.shape[1]))  # list of matrix to store path flows
-
-    for path in allpaths:
+    print('constructing edge formulations...')
+    for path in tqdm(allpaths, total=num_variables):
         path_array_tmp = np.zeros(G.adj.shape)
         index_x = [path[k] for k in range(len(path) - 1)]  # x index of the adj matrix
         index_y = [path[k] for k in range(1, len(path))]  # y index of the adj matrix
@@ -175,20 +196,19 @@ def _system_optimal_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
     # element (i, j) is the total flow on edge (i,j)
     allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis=0)
     total_cost = allflows * linear_cost(allflows, a)
-    obj_fun = linear_SO_obj(allflows, a).sum()
+    obj_fun = linear_so_obj(allflows, a).sum()
 
-    print('The initial cost is %f, and the initial flow is ' % (obj_fun), x)
-    if num_variables < 2:
-        #G.SOflowsLinear[s, t] = 1
-        #G.SOcostsLinear[s, t] = obj_fun
-        #G.SOflowsLinear_edge += allflows
-        #G.SOcostsLinear_edge += total_cost
-        return x, allflows, obj_fun, total_cost
-
+    print('The initial flow (path formulation) is ', x)
+    print('The initial cost is %f' % obj_fun)
     print('------solve the system optimal flow------')
 
+    if num_variables < 2:
+        print('System Optimal flow found:', x)
+        print('The total travel time is %f' % obj_fun)
+        return x, allflows, obj_fun, total_cost
+
     gradients = np.array(
-        [np.sum( linear_cost(allflows, a) * (
+        [np.sum(linear_cost(allflows, a) * (
 
                     path_arrays[k] * np.where(path_arrays[-1] == 0, 1, 0) - np.where(path_arrays[k] == 0, 1, 0) *
                     path_arrays[-1]))
@@ -201,11 +221,11 @@ def _system_optimal_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
     )
 
     # initial step size determination
-    gamma1 = np.min( np.abs( x[:-1]/gradients ) )
-    gamma2 = np.min( np.abs( (1 - x[:-1])/ gradients ) )
-    gamma = min(gamma1, gamma2) * 2  / 3
+    gamma1 = np.min(np.abs(x[:-1]/gradients))
+    gamma2 = np.min(np.abs((1 - x[:-1]) / gradients))
+    gamma = min(gamma1, gamma2) * 2 / 3
 
-    for k in range(maximum_iter):  # maximal iteration 10000
+    for k in tqdm(range(maximum_iter)):  # maximal iteration 10000
         prev_x = np.copy(x)
         prev_gradients = np.copy(gradients)
 
@@ -229,11 +249,11 @@ def _system_optimal_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
 
         allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis=0)
         total_cost = allflows * linear_cost(allflows, a)
-        obj_fun = linear_SO_obj(allflows, a).sum()
+        obj_fun = linear_so_obj(allflows, a).sum()
 
         # new gradients and gamma 
         gradients = np.array(
-            [np.sum( linear_cost(allflows, a) * (
+            [np.sum(linear_cost(allflows, a) * (
                         path_arrays[k] * np.where(path_arrays[-1] == 0, 1, 0) - np.where(path_arrays[k] == 0, 1, 0) *
                         path_arrays[-1]))
              for k in range(num_variables - 1)]
@@ -244,17 +264,16 @@ def _system_optimal_linear_solve(G, s, t, tol, maximum_iter, allpaths, a):
              for k in range(num_variables - 1)]
         )
         
-        if np.sum(np.where(np.abs(gradients-prev_gradients) < tol , 0, 1)) == 0:
-            #G.SOflowsLinear[s, t] = 1
-            #G.SOcostsLinear[s, t] = obj_fun
-            #G.SOflowsLinear_edge += allflows
-            #G.SOcostsLinear_edge += total_cost
-            print('System optimum found:')
-            print('Iteration %d: The total cost is %f, and the flow is ' % (k, obj_fun), x)
+        if np.sum(np.where(np.abs(gradients-prev_gradients) < tol, 0, 1)) == 0:
+            print('System Optimal flow found:', x)
+            print('Iteration %d: the total travel time is %f' % (k, obj_fun))
             return x, allflows, obj_fun, total_cost
 
-        gamma = np.inner(x[:-1] - prev_x[:-1], gradients - prev_gradients) \
-                / np.inner(gradients - prev_gradients, gradients - prev_gradients)
+        gamma = np.inner(
+            x[:-1] - prev_x[:-1], gradients - prev_gradients
+        ) / np.inner(
+            gradients - prev_gradients, gradients - prev_gradients
+        )
 
-    print('global minimum not found')
+    print('System Optimal flow not found')
     return
