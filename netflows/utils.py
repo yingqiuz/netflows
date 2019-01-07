[FIX] modify gradients dimension

netflows/utils.py
@@ -84,7 +84,7 @@ def we_affine_grad(x, a, a0, path_arrays, num_variables):
     allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis = 0)
     gradients = np.array(
         [np.sum(affine_cost(allflows, a, a0) * path_arrays[k] )
-         for k in range(num_variables)]
+         for k in range(num_variables - 1)]
     )
     return gradients
 
@@ -94,17 +94,17 @@ def so_affine_grad(x, a, a0, path_arrays, num_variables):
     gradients = np.array(
         [np.sum(affine_cost(allflows, a, a0) *
                 path_arrays[k] )
-         for k in range(num_variables)]
+         for k in range(num_variables - 1)]
     ) + np.array(
         [np.sum(allflows * a *
                 path_arrays[k])
-         for k in range(num_variables)]
+         for k in range(num_variables - 1)]
     )
     return gradients
 
 
 def we_linear_grad(x, a, path_arrays, num_variables):
-    allflows = np.sum(path_arrays * np.append(x, 1 - np.sum(x)).reshape(num_variables, 1, 1), axis=0)
+    allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis=0)
     gradients = np.array(
         [np.sum(linear_cost(allflows, a) * (path_arrays[k] * np.where(path_arrays[-1] == 0, 1, 0) -
                                             np.where(path_arrays[k] == 0, 1, 0) * path_arrays[-1]))
@@ -114,7 +114,7 @@ def we_linear_grad(x, a, path_arrays, num_variables):
 
 
 def so_linear_grad(x, a, path_arrays, num_variables):
-    allflows = np.sum(path_arrays * np.append(x, 1 - np.sum(x)).reshape(num_variables, 1, 1), axis=0)
+    allflows = np.sum(path_arrays * x.reshape(num_variables, 1, 1), axis=0)
     gradients = np.array(
         [np.sum(linear_cost(allflows, a) * (
                 path_arrays[k] * np.where(path_arrays[-1] == 0, 1, 0) - np.where(path_arrays[k] == 0, 1, 0) *